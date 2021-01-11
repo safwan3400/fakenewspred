620e8a0086777e0118e01080f66755b3d528d67f
Mohammed safwan <53251928+safwan3400@users.noreply.github.com>
2021-01-11 10:24:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request
 import jsonify
 import requests
-import pickle
 import numpy as np
 import sklearn
 from sklearn.preprocessing import StandardScaler
